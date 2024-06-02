8dbed77977ec494094f2204e589bf0f35a46e3ab
Sanyi81 <skyharts2002@gmail.com>
2024-06-02 23:32:20 +0300

    init lesson9

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
+import {AuthDataModel} from "../models/AuthDataModel";
+import {authService} from "../services/api.service";
+
+const FormComponent = () => {
+    const {
+        handleSubmit,
+        register
+    } = useForm<AuthDataModel>(
+            {defaultValues: {username: 'Sanyi81', password: 'S@nyi'}}
+    );
+
+    const [isAuthState, setIsAuthState] = useState<boolean>(false);
+
+    const authenticate = async (formData:AuthDataModel):Promise<void> => {
+        const isAuth = await authService.authentication(formData);
+        setIsAuthState(isAuth);
+    };
+    return (
+        <div>
+            <h3>Login form</h3>
+            <div>
+                {isAuthState? <span>OK</span> : <span>Not OK</span>}
+            </div>
+            <form onSubmit={handleSubmit(authenticate)}>
+            <input type="text" {...register('username')}/>
+            <input type="text" {...register('password')}/>
+        </form>
+        </div>
+    );
+};
+
+export default FormComponent;
